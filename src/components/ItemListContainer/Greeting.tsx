03201a2d806bb8a3a7ddf9c1cc526539299b30cb
cotanol <salcedomoralesfranciscojose@gmail.com>
2024-12-08 14:03:48 -0500

    Un poco de estilo

--- src/components/ItemListContainer/Greeting.tsx
+++ src/components/ItemListContainer/Greeting.tsx
@@ -7,8 +7,10 @@ interface IGreeting {
 const Greeting =  ({saludo = "Bievenido"}: IGreeting) => {
 
     return  (
-        <div>
-            <h1> {saludo} </h1>
+        <div className="flex justify-center items-center h-screen bg-gray-100">
+            <h1 className="text-xl font-medium text-white bg-black p-4 rounded-lg mt-10"> 
+                {saludo} 
+            </h1>
         </div>
 
     )
